71ffc6b9da45c0d39a67e0448774190de8affd79
Takuya Kinoshita <kktak02@gmail.com>
2023-11-05 10:30:42 +0900

    template ongoing

--- main.go
+++ main.go
@@ -2,23 +2,27 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 )
 
 func main() {
 
-	// マルチプレクサを使ってリクエストを受け付ける
-	// mux := http.NewServeMux()
+	//マルチプレクサを使ってリクエストを受け付ける
+	mux := http.NewServeMux()
 
-	// mux.HandleFunc("/", index)
-	// mux.HandleFunc("/login", login)
-	// http.ListenAndServe("0.0.0.0:8080", mux)
+	mux.HandleFunc("/", index)
+	mux.HandleFunc("/login", login)
+	http.ListenAndServe("0.0.0.0:8080", mux)
 
 }
 
 // Handlerの定義
 func index(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "hello world")
+	// fmt.Fprintf(w, "hello world")
+	t, _ := template.ParseFiles("templates/index.html")
+	t.Execute(w, "helloooo!!!")
+
 }
 
 // GET /login
